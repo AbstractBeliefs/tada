1a181d0c63707f490ef94b9ed2381990f480bb3e
AbstractBeliefs <abstractbeliefs@gmail.com>
2014-02-08 23:27:06 +0000

    Tab-to-space fix

--- templates/adium.py
+++ templates/adium.py
@@ -1,27 +1,35 @@
-<?xml version="1.0" encoding="UTF-8"?>
+from yapsy.IPlugin import IPlugin
+import jinja2
+import zipfile
+
+
+class AdiumBackend(IPlugin):
+    
+    template = \
+"""<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
 <dict>
-	<key>AdiumSetVersion</key>
-	<integer>1</integer>
+    <key>AdiumSetVersion</key>
+    <integer>1</integer>
 
-	<key>Emoticons</key>
+    <key>Emoticons</key>
     <dict>
 
         {% for file in Emotes.emotelist %}
-		<key>{{ file.filename }}</key>
-		<dict>
-			<key>Equivalents</key>
-			<array>
+        <key>{{ file.filename }}</key>
+        <dict>
+            <key>Equivalents</key>
+            <array>
                 {% for shortcut in file.shortcuts %}
-				<string>{{ shortcut }}</string>
+                <string>{{ shortcut }}</string>
                 {% endfor %}
-			</array>
-			<key>Name</key>
-			<string>{{ file.filename }}</string>
-		</dict>
+            </array>
+            <key>Name</key>
+            <string>{{ file.filename }}</string>
+        </dict>
         {% endfor %}
 
     </dict>
 </dict>
-</plist>
+</plist>"""
